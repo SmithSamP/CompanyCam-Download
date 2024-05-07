7ad13266668e5baa1252847a7408da50ef83a0df
Samuel Smith <samuel.rascal@gmail.com>
2024-05-06 21:37:48 -0700

    Simplified methods

--- project.py
+++ project.py
@@ -2,24 +2,11 @@ import requests
 import json
 import pandas as pd
 import datetime
-import piexif
+# import piexif
 from progress.bar import Bar
 
-class CompanyCamProject:
-    """
-    A class representing a CompanyCam project with methods for CRUD operations.
-    Requires a valid CompanyCam API access token for authentication.
-    """
-
+class CompanyCam:
     def __init__(self, api_token, base_url="https://api.companycam.com/v2"):
-        """
-        Initializes a CompanyCamProject object.
-
-        Args:
-            api_token (str): Your CompanyCam API access token.
-            base_url (str, optional): Base URL for the CompanyCam API (defaults to "https://api.companycam.com/v2").
-        """
-
         self.api_token = api_token
         self.base_url = base_url
         self.name = None
@@ -37,16 +24,6 @@ class CompanyCamProject:
 
 
     def get_project(self, project_id):
-        """
-        Gets details of a specific CompanyCam project.
-
-        Args:
-            project_id (str): ID of the project to retrieve.
-
-        Returns:
-            dict: Dictionary containing project details if successful,
-                  raises an exception on failure.
-        """
         # construct the URL for the API call
         url = f"{self.base_url}/projects/{project_id}"
         # make the API call
@@ -60,133 +37,25 @@ class CompanyCamProject:
         # return the project details
         return project_data
     
-    def get_project_photos(self, project_id, start_date=None, end_date=None):
-        """
-        Gets photos for a specific CompanyCam project.
-
-        Args:
-            project_id (str): ID of the project to retrieve photos for.
-
-        Returns:
-            Pandas dataFrame containing photo details if successful,
-                  raises an exception on failure.
-        """
-        # initialize an empty dataFrame to store photos data
-        photos_dataFrame = pd.DataFrame(columns=['id', 'url', 'date', 'user', 'project', 'lat', 'lon'])
-
-        # loop through pages of photos data
-        for index in range(1,10000):
-            # construct the URL for the API call
-            url = f"{self.base_url}/projects/{project_id}/photos?page={index}&per_page=1000"
-
-            if start_date != None:
-                url += f"&start_date={start_date}"
-
-            if end_date != None:
-                url += f"&end_date={end_date}"
-            # make the API call
-            page_data = self.make_api_call(url)
-            # convert the dictionary to a pandas dataFrame
-            if page_data is None:
-                break
-            # if the dataFrame is empty, assign the current page data to it otherwise concatenate the data
-            if photos_dataFrame.empty:
-                photos_dataFrame = page_data
-                continue
-            photos_dataFrame = pd.concat([photos_dataFrame, page_data], ignore_index=True)
-            print(f"Retrieved page {index} of photos")
-        # return the photos dataFrame
-        return photos_dataFrame
-    
-    def get_group_photos(self, group_id, start_date=None, end_date=None):
-        """
-        Gets photos for a specific CompanyCam group.
-
-        Args:
-            group_id (str): ID of the group to retrieve photos for.
-
-        Returns:
-            Pandas dataFrame containing photo details if successful,
-                  raises an exception on failure.
-        """
-        # initialize an empty dataFrame to store photos data
-        photos_dataFrame = pd.DataFrame(columns=['id', 'url', 'date', 'user', 'project', 'lat', 'lon'])
-
-        # loop through pages of photos data
-        for index in range(1,10000):
-            # construct the URL for the API call
-            url = f"{self.base_url}/photos?page={index}&per_page=1000&group_ids={group_id}"
-
-            if start_date != None:
-                url += f"&start_date={start_date}"
-
-            if end_date != None:
-                url += f"&end_date={end_date}"
-
-            # make the API call
-            page_data = self.make_api_call(url)
-            # convert the dictionary to a pandas dataFrame
-            print(f"Retrieved page {index} of photos")
-            if page_data is None:
-                break
-            # if the dataFrame is empty, assign the current page data to it otherwise concatenate the data
-            if photos_dataFrame.empty:
-                photos_dataFrame = page_data
-                continue
-            photos_dataFrame = pd.concat([photos_dataFrame, page_data], ignore_index=True)
-
-        # return the photos dataFrame
-        return photos_dataFrame
-    
     def get_groups(self):
         url = f"{self.base_url}/groups?per_page=1000"
         response = self.get_response(url)
         groups_data = response.json()
         groups_df = pd.DataFrame(groups_data)
-        groups_df = groups_df[['id', 'name']]
         return groups_df
     
-    def make_api_call(self,url):
-        response = self.get_response(url)
-        photos_data = response.json()
-        # break the loop if no more photos are available on current page
-        if photos_data == []:
-            return None
-        # Initialize a dictionary to store data for the current page
-        # function to convert timestamp to readable date
-        page_df = pd.DataFrame(photos_data)
-
-        page_df['url'] = page_df['uris'].apply(lambda x: x[3]['uri'])
-        page_df['lat'] = page_df['coordinates'].apply(lambda x: x['lat'])
-        page_df['lon'] = page_df['coordinates'].apply(lambda x: x['lon'])
-        # loop through photos data and store relevant information in the dictionary
-        return page_df
+    def get_project_photos(self, project_id, start_date=None, end_date=None):
+        url = f"{self.base_url}/projects/{project_id}/photos?page=***&per_page=1000"
+        photos_dataFrame = self.build_dataframe(url, start_date, end_date)
+        # return the photos dataFrame
+        return photos_dataFrame
     
-    def download_photo(self, photo_url, filename):
-        """
-        Downloads a photo from a given URL and saves it to a file.
-
-        Args:
-            photo_url (str): URL of the photo to download.
-            filename (str): Name of the file to save the photo to.
-
-        Returns:
-            bool: True if successful, False otherwise.
-        """
-        # make the API call to download the photo
-        response = requests.get(photo_url)
-        if response.status_code == 200:
-            with open(filename, "wb") as f:
-                f.write(response.content)
-
-            exif_dict = piexif.load(filename)  # Load EXIF data
-            # ... You can also modify EXIF data here if needed ... 
-            exif_bytes = piexif.dump(exif_dict)
-            piexif.insert(exif_bytes, filename)
-            return True
-        else:
-            return False
-        
+    def get_group_photos(self, group_id, start_date=None, end_date=None):
+        url = f"{self.base_url}/photos?page=***&per_page=1000&group_ids={group_id}"
+        photos_dataFrame = self.build_dataframe(url, start_date, end_date)
+        # return the photos dataFrame
+        return photos_dataFrame
+    
     def get_response(self, url):
         try:
             response = requests.get(url, headers=self.headers)
@@ -198,6 +67,58 @@ class CompanyCamProject:
         except requests.exceptions.ConnectionError as err:
             print(f'Could not connect. Error: {err}')
             return None
+        
+    def make_api_call(self,url):
+        response = self.get_response(url)
+        photos_data = response.json()
+        # break the loop if no more photos are available on current page
+        if photos_data == []:
+            return None
+        # Initialize a dictionary to store data for the current page
+        # function to convert timestamp to readable date
+        page_df = pd.DataFrame(photos_data)
+        # loop through photos data and store relevant information in the dictionary
+        return page_df
+
+    def build_dataframe(self, url, start_date=None, end_date=None):
+        photos_dataFrame = pd.DataFrame()
+        if start_date != None:
+            url += f"&start_date={start_date}"
+
+        if end_date != None:
+            url += f"&end_date={end_date}"
+
+        for index in range(1,10000):
+            # make the API call
+            indexed_url = url.replace('***', str(index))
+            page_data = self.make_api_call(indexed_url)
+            # convert the dictionary to a pandas dataFrame
+            print(f"Retrieved page {index} of photos")
+            if page_data is None:
+                break
+            # if the dataFrame is empty, assign the current page data to it otherwise concatenate the data
+            if photos_dataFrame.empty:
+                photos_dataFrame = page_data
+                continue
+            photos_dataFrame = pd.concat([photos_dataFrame, page_data], ignore_index=True)
+
+        # return the photos dataFrame
+        return photos_dataFrame
+        
+    def download_photo(self, photo_url, filename):
+        # make the API call to download the photo
+        response = requests.get(photo_url)
+        if response.status_code == 200:
+            with open(filename, "wb") as f:
+                f.write(response.content)
+
+            # exif_dict = piexif.load(filename)  # Load EXIF data
+            # # ... You can also modify EXIF data here if needed ... 
+            # exif_bytes = piexif.dump(exif_dict)
+            # piexif.insert(exif_bytes, filename)
+            return True
+        else:
+            return False
         
 
 
@@ -211,15 +132,13 @@ if __name__ == "__main__":
 
     # Assign token to variable
     project_id = '62018691'
-    ccproject = CompanyCamProject(token)
+    ccproject = CompanyCam(token)
     ccproject.get_project(project_id)
     # photos = ccproject.get_project_photos(project_id, start_time, end_time)
     print(ccproject.get_groups())
     group_id = '11083'
     photos = ccproject.get_group_photos(group_id, start_time, end_time)
 
-    # Write photos to a CSV file
-    photos.to_csv('photos.csv', index=False)
     with Bar('Processing...',suffix='%(percent)d%%- %(eta)ds',max=len(photos)) as bar:
         for index, row in photos.iterrows():
             filename = 'D:\\Photos\\'+f"photo_{row['id']}.jpg"
